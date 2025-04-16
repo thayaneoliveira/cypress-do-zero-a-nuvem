9dd22285b179ce1890e1d63532f94b6fff699746
Seu Nome <seu-email@exemplo.com>
2025-04-16 14:24:07 -0400

    Initial commit

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,7 @@
+//describe(suit de testes )
+describe("Central de Atendimento ao Cliente TAT", () => {
+  it("verifica o título da aplicação", () => {
+    cy.visit("./src/index.html");
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+});
