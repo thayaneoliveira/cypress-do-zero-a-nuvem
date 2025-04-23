faf5dd2b79e9b0f7930e01f6daff6584729b4a72
Thayane Oliveira <thay.skaywalker@gmail.com>
2025-04-23 09:59:00 -0400

    atualização

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 describe("Central de Atendimento ao Cliente TAT", () => {
   //antes de casa teste esse beforeEach é executado
   beforeEach(() => {
@@ -145,5 +144,27 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .should((input) => {
         expect(input[0].files[0].name).to.equal("example.json");
       });
+  });
+
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
+    cy.get("#file-upload")
+      .selectFile("cypress/fixtures/example.json")
+      .should((input) => {
+        expect(input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+    cy.contains("a", "Política de Privacidade")
+      .should("have.attr", "href", "privacy.html")
+      .and("have.attr", "target", "_blank");
+  });
+
+  it.only("acessa a página da política de privacidade removendo o target e então clicando no link", () => {
+    cy.contains("a", "Política de Privacidade")
+      .invoke("removeAttr", "target")
+      .click();
+
+    cy.contains("h1", "CAC TAT - Política de Privacidade");
   });
 });
